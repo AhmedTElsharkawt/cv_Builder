9b76668067cac4334d7bee3b7cdfe99a094513a7
Ahmed_Sharqawy <a0202t@gmail.com>
2025-06-18 18:58:44 +0300

    improve some codes

--- resources/views/cvbuilder/templates/index.blade.php
+++ resources/views/cvbuilder/templates/index.blade.php
@@ -11,9 +11,10 @@
                     <img src="{{ asset($template->preview_image) }}" class="card-img-top" alt="Preview">
                     <div class="card-body text-center">
                         <a href="{{ route('cvbuilder.templates.show', $template->name) }}" class="btn btn-primary">معاينة القالب</a>
-                        <a href="{{ route('cvbuilder.templates.download', $template->name) }}" class="btn btn-primary">تحميل القالب</a>
                         @if ($template->type === 'paid' && !auth()->user()->cv_hasPurchasedTemplate($template->id))
-                        <a href="{{ route('cvbuilder.templates.purchase', $template->id) }}" class="btn btn-primary">شراء القالب</a>
+                            <a href="{{ route('cvbuilder.templates.purchase', $template->id) }}" class="btn btn-primary">شراء القالب</a>
+                        @else
+                            <a href="{{ route('cvbuilder.templates.download', $template->name) }}" class="btn btn-primary">تحميل القالب</a>
                         @endif
                     </div>
 
