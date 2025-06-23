4d9e7abfff5a6e8fabd29eeaf433af5fd0f94f2c
Ahmed_Sharqawy <a0202t@gmail.com>
2025-06-23 23:33:37 +0300

    edit temlates

--- resources/views/cvbuilder/templates/index.blade.php
+++ resources/views/cvbuilder/templates/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="card h-100">
                     <img src="{{ asset($template->preview_image) }}" class="card-img-top" alt="Preview">
                     <div class="card-body text-center">
-                        <a href="{{ route('cvbuilder.templates.show', $template->name) }}" class="btn btn-primary">معاينة القالب</a>
+                        <a href="{{ route('cvbuilder.templates.show', $template->name) }}" class="btn btn-primary" target="_blank">معاينة القالب</a>
                         @if ($template->type === 'paid' && !auth()->user()->cv_hasPurchasedTemplate($template->id))
                             <a href="{{ route('cvbuilder.templates.purchase', $template->id) }}" class="btn btn-primary">شراء القالب</a>
                         @else
